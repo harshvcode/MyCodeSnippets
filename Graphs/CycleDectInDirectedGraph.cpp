0662acc2b9c5388176f8c27c923bbcc0d055522a
Harshvardhan Singh <51012630+harshvcode@users.noreply.github.com>
2020-08-30 08:47:47 +0530

    Update CycleDectInDirectedGraph.cpp

--- Graphs/CycleDectInDirectedGraph.cpp
+++ Graphs/CycleDectInDirectedGraph.cpp
@@ -7,30 +7,24 @@ using namespace std;
 
 vector<int> adj[8];
 vector<bool> vis(8, 0);
-vector<int> color(8, 0);
-bool Cycfound=0;
-void dfs(int v, int p){
-  color[v] = 1; // GREY
-  for(int w : adj[v]){
-    if(color[w] == 1){
-    	Cycfound=1;
-    	return;
-    }
-    if(color[w] == 0) dfs(w, v);
-  }
-  color[v] = 2; // BLACK
-}
-bool iscyclic(int V){
-	Cycfound=0;
-	for(int i = 0; i < V; i++)
-		if(color[i] == 0) dfs(i, -1); // IF NODE IS WHITE, START NEW DFS
-	return Cycfound;
+vector<int> color(8, 0);//Default Color 0 = White
+bool Cycfound=0;//Becomes true if there is a cycle
+void checkCycle(int v){
+	color[v] = 1; // GREY
+  	for(int w : adj[v]){
+    	if(color[w] == 1){
+    		Cycfound=1;
+    		return;
+    	}
+    	if(color[w] == 0) checkCycle(w);
+  	}
+  	color[v] = 2; // BLACK
 }
 
 int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	//freopen("inp.txt","r",stdin);
+	freopen("inp.txt","r",stdin);
 	adj[0].push_back(1);
 	adj[0].push_back(2);
 	adj[0].push_back(3);
@@ -38,7 +32,8 @@ int main(){
 	adj[1].push_back(5);
 	adj[5].push_back(6);
 	adj[6].push_back(7);
-	adj[7].push_back(1);
-	cout << iscyclic(8);//8 is total size
+	adj[7].push_back(2);
+	checkCycle(0);
+	cout << Cycfound;
 	return 0;
 }
