7a17c472a2be802e050ea9873f0c0959a1be9b76
Harshvardhan Singh <51012630+harshvcode@users.noreply.github.com>
2020-09-07 18:15:04 +0530

    Update CycleDetecInUndirec.cpp

--- Graphs/CycleDetecInUndirec.cpp
+++ Graphs/CycleDetecInUndirec.cpp
@@ -1,76 +1,89 @@
-/*
-  By  : harshvcode
- */
 #include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define ld long double
 #include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-#define VT vector
-#define PB push_back
-#define ALL(c) (c).begin(),(c).end()
-#define SZ(x) (int)(x).size()
-#define FR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
-#define ECH(x, a) for (auto& x: a)
-#define F first
-#define S second
-mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
-ll randnum(ll a, ll b) {
-	return uniform_int_distribution<ll>(a, b)(mt_rng);
+using namespace std;
+
+#define ff first
+#define ss second
+#define int long long
+#define f(i,a,b,o) for(auto i=a;(o>0)?i<=b:i>=b;i+=o)
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define vi vector<int>
+#define mii map<int,int>
+#define pqb priority_queue<int>
+#define pqs priority_queue<int,vi,greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x) int x; cin>>x; while(x--)
+#define full(x) (x).begin(),(x).end()
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+void pre(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("inp.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	#endif
 }
-const int n=4;
-VT<int> adj[n];
-queue<int> q;
-VT<bool> vis(n, 0);
-void dfs(int v){
-	if(vis[v]) return;
+
+vi edges[8];
+vector<bool> vis(8, 0);
+vector<int> color(8, 0);//default colour is 0(white)
+bool cyc=0;//becomes 1 if cycle found
+
+void add(int a, int b){
+	edges[a].pb(b);
+	edges[b].pb(a);
+}
+void detecCycleInUnd(int v){
+	vis[v]=1;
+	color[v]=1;//grey
 	cout << v << ' ';
-	vis[v]=true;
-	for(auto e:adj[v])
-		dfs(e);
+	for(auto e:edges[v]){
+		if(color[e] == 0) detecCycleInUnd(e);
+		else if(color[e] == 2) cyc=1;
+	}
+	color[v]=2;//black
 }
+int32_t main()
+{
+	pre();
+	add(0, 1);
+	add(0, 2);
+	add(0, 3);
+	add(1, 4);
+	add(1, 5);
+	add(5, 6);
+	add(6, 7);
+	add(7, 5);
+	//add(1, 2);
+	//add(3, 0);
+	detecCycleInUnd(0);cout << '\n' << "ans: ";
+	cout << cyc;
+	//add(7, 3);
+	return 0;
+}
+/*
 void bfs(int v){
-	vis[v]=true;
+	vis[v]=1;
+	queue<int> q;
 	q.push(v);
 	while(! q.empty()){
 		v=q.front();
-		cout << v << ' ';
+		//cout << v << '>';
 		q.pop();
-		for(auto e:adj[v]){
-			if(vis[e]) continue;
-			vis[e]=1;
-			q.push(e);
+		for(int e:edges[v]){
+			if(! vis[e]){
+				vis[e]=1;
+				q.push(e);
+			}
 		}
 	}
 }
-void add(int v1, int v2){
-	adj[v1].PB(v2);
-	adj[v2].PB(v1);
-}
-//Detect cycle in undirected graph, But any vertex must not be -1
-//as i have used parent value default as -1,
-//Other wise we have to use a bool value for first time recursion call
-bool detecCycleInUnd(int v, int parent=-1){
-	if(v != parent && vis[v]) return 1;
-	vis[v]=1;
-	for(auto e:adj[v]){
-		if(e != parent){
-			if(vis[e]) return 1;
-			else return detecCycleInUnd(e, v);
-		}
-	}
-	return 0;
-}
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	add(0, 1);
-	add(2, 0);
-	add(1, 3);
-	//add(3, 2); This line makes cycle, see what happens by commenting and uncommenting this line.
-	cout << detecCycleInUnd(0);
-	return 0;
-}
+*/
