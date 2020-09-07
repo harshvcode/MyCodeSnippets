3ef1c44c85e836c9571f5226879c86582b569e2d
Harshvardhan Singh <51012630+harshvcode@users.noreply.github.com>
2020-09-07 19:11:22 +0530

    Update CycleDectInDirectedGraph.cpp

--- Graphs/CycleDectInDirectedGraph.cpp
+++ Graphs/CycleDectInDirectedGraph.cpp
@@ -1,39 +1,38 @@
-/*
-__________By__________
-______harshvcode______
-*/
-#include <bits/stdc++.h>
-using namespace std;
-
-vector<int> adj[8];
-vector<bool> vis(8, 0);
-vector<int> color(8, 0);//Default Color 0 = White
-bool Cycfound=0;//Becomes true if there is a cycle
-void checkCycle(int v){
-	color[v] = 1; // GREY
-  	for(int w : adj[v]){
-    	if(color[w] == 1){
-    		Cycfound=1;
-    		return;
-    	}
-    	if(color[w] == 0) checkCycle(w);
-  	}
-  	color[v] = 2; // BLACK
+bool cyclic(int i, int V, vector<int> adj[],bool visited[], bool inTheCall[])
+{
+  
+    visited[i]=true;
+    inTheCall[i]=true;
+    for(int j =0;j<adj[i].size();j++)
+    { 
+        if(!visited[adj[i][j]] &&  cyclic(adj[i][j],V,adj,visited,inTheCall))
+        {
+            return true;
+        }
+        else{
+            if(inTheCall[adj[i][j]])
+            return true;
+        }
+    }
+    
+    inTheCall[i]=false;
+    return false;
 }
-
-int main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	freopen("inp.txt","r",stdin);
-	adj[0].push_back(1);
-	adj[0].push_back(2);
-	adj[0].push_back(3);
-	adj[1].push_back(4);
-	adj[1].push_back(5);
-	adj[5].push_back(6);
-	adj[6].push_back(7);
-	adj[7].push_back(2);
-	checkCycle(0);
-	cout << Cycfound;
-	return 0;
+bool isCyclic(int V, vector<int> adj[])
+{
+    bool visited[V];
+    bool inTheCall[V];
+    for(int  i=0;i<V;i++)
+    {
+        visited[i]=false;
+        inTheCall[i]= false;
+    }
+    
+    for(int i =0;i<V;i++)
+    {  if(!visited[i]){
+      if(cyclic(i,V,adj,visited,inTheCall))
+      return true;
+    }
+    }
+    return false;
 }
