d55c0fad987c63dcb3f9cc7d8fc3e3dce0b26371
Harshvardhan Singh <51012630+harshvcode@users.noreply.github.com>
2021-03-24 10:15:13 +0530

    Update Graph_undir.java

--- Graphs/Graph_undir.java
+++ Graphs/Graph_undir.java
@@ -1,4 +1,5 @@
-import java.util.*;
+//harshvcode
+import java.util.*;	
 import java.io.*;
 
 class Graph {
@@ -15,24 +16,173 @@ class Graph {
 	}
 	void add(int a, int b) {
 		graph.get(a).add(b);
-		graph.get(b).add(a);//comment if graph is directed
+		//graph.get(b).add(a);//comment if graph is directed
 		
 	}
+	/*************DFS*******************/
 	void dfs(int x) {
+		Stack<Integer> st = new Stack<>();
+		st.push(x);
 		looked[x] = true;
-		System.out.print(x + " ");
-		for(Integer vert : graph.get(x)) {
-			if(! looked[vert])
-				dfs(vert);
+		while(! st.isEmpty()) {
+			x = st.pop();
+			System.out.print(x + " ");
+			for(int e : graph.get(x)) {
+				if(! looked[e]) {
+					st.push(e);
+					looked[e] = true;
+				}
+			}
 		}
 	}
+	/*********************BFS*********/
+	void bfs(int x) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(x);
+		looked[x] = true;
+		while(! q.isEmpty()) {
+			x = q.poll();// This method retrieves and removes the head.
+			System.out.print(x + " ");
+			for(int e : graph.get(x)) {
+				if(! looked[e]) {
+					q.add(e);
+					looked[e] = true;
+				}
+			}
+		}
+	}
+	/******************MST Basic*********/
+	void mst(int x) {
+		/*Input
+		0 1
+		0 2
+		1 3
+		2 4
+		3 5
+		4 5
+		4 6
+		5 6
+		*/
+		Stack<Integer> st = new Stack<>(), mst_trv = new Stack<>();
+		st.push(x);
+		looked[x] = true;
+		mst_trv.push(x);
+		while(! st.isEmpty()) {
+			x = st.pop();
+			for(int e : graph.get(x)) {
+				if(! looked[e]) {
+					st.push(e);
+					mst_trv.push(e);
+					looked[e] = true;
+				}
+			}
+		}
+		//Printing minimum spanning tree vertices
+		while(! mst_trv.isEmpty()) {
+			System.out.print(mst_trv.pop() + " ");
+		}
+		System.out.println();
+	}
+	
+	/*************See Graph**********/
+	
 	void visu() {//to see what's the adjacency list contains
 		System.out.println(graph);
 	}
+	
+	/*****************Reset Looked to 0*************/
 	void reset_looked(boolean print_ln) {//sets all vertices to unlooked
 		if(print_ln) System.out.println();
 		for(int i = 0; i < max_size; ++i) looked[i] = false;
 	}
+	
+	/**********Cycle Detection Undirected Graph*************/
+	boolean cycle_det_und() {//call this
+		/*Input
+		0 1
+		0 6
+		1 2
+		2 4
+		2 7
+		3 7
+		5 6
+		5 3//comment or uncomment to see effect
+		6 5
+		7 2
+		7 3
+		*/
+		reset_looked(false);
+		for(int i = 0; i < max_size; ++i) {
+			if(looked[i] == false && Helper_cycle_det_und(i, -1) == true)
+				return true;
+		}
+		return false;
+	}
+	boolean Helper_cycle_det_und(int curr, int par) {//abstract helper
+		looked[curr] = true;
+		for (int i = 0; i < graph.get(curr).size() ; ++i) {
+				List<Integer> list = new ArrayList<>(graph.get(curr));//hashset to list
+                int vertex = list.get(i);
+                //check the adjacent vertex from current vertex
+                if(vertex != par) {
+                    //if destination vertex is not its direct parent then
+                    if(looked[vertex]){
+                        //if here means this destination vertex is already visited
+                        //means cycle has been detected
+                        return true;
+                    }
+                    else{
+                        //recursion from destination node
+                        if (Helper_cycle_det_und(vertex, curr)) {
+                            return true;
+                        }
+                    }
+                }
+        }
+        return false;
+	}
+	
+	/**********Cycle Detection Directed Graph*************
+	Input:
+		0 1
+		0 2
+		0 3
+		1 2
+		3 4
+		4 5
+		5 3  Comment or uncomment to see effect
+		*/
+	boolean cycle_found=false;
+	int[] color_box;
+	void color_ini() {
+		cycle_found = false;
+		reset_looked(false);
+		color_box = new int[max_size];
+		for(int i = 0; i < max_size; ++i) color_box[i] = 0;
+		//System.out.println("Hey " + Arrays.toString(color_box));
+	}
+	void cycle_det_dir(int x) {
+		//System.out.println("a");
+		looked[x] = true;
+		//System.out.println("b");
+  		color_box[x] = 1;
+  		//System.out.println("c");
+  		for(int e: graph.get(x)) {
+  			//System.out.println("d");
+  			if(looked[e] == false && color_box[e] == 0) {
+  				//System.out.println("e");
+				cycle_det_dir(e);
+			}
+			else if(color_box[e] == 1) {
+				//System.out.println("f");
+				cycle_found = true;
+				return;
+			}
+		}
+		//System.out.println("g");
+		color_box[x] = 0;
+	}
+	/*************Use till here*************/
 }
 
 class Main {
@@ -47,15 +197,14 @@ class Main {
 		4 5
 		4 6
 		*/
-		Graph gg = new Graph(7);
+		Graph gg = new Graph(6);
 		for(int i = 0; i < 7; ++i) {
 			gg.add(Reader.nextInt(), Reader.nextInt());
 		}
-		gg.dfs(0);
-		gg.reset_looked(true);
-		gg.dfs(6);
-		System.out.println();
-		gg.visu();
+		gg.color_ini();
+		System.out.println(gg.cycle_found);
+		gg.cycle_det_dir(0);
+		System.out.println(gg.cycle_found);
 	}
 }
 
