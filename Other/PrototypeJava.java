87d1c6a92c20025499f4879d598c30a2e166462b
Harshvardhan Singh <51012630+dedidec223@users.noreply.github.com>
2021-03-28 21:16:48 +0530

    Update PrototypeJava.java

--- Other/PrototypeJava.java
+++ Other/PrototypeJava.java
@@ -1,69 +1,119 @@
-/*
- dedidec | hvsreal.223@gmail.com
-*/
-import java.util.*;
 import java.io.*;
-class Temp {
-  int mod = (int)1e9 + 8;
-  long inf = (long)1e18;
-  void nl() { System.out.println(); }
-  long stbits(long n) { return Long.bitCount(n); }
-  long zrbits(long n) { return 32 + (32 - Long.numberOfLeadingZeros(n) - stbits(n)); }
-  void ssp(int[] s,char d) { for(int e : s) System.out.print(e + ""+ d); nl(); }
-  void ssp(int[] s,int n,char d) { for(int i=0; i<n; ++i) System.out.print(s[i] + ""+ d); nl(); }
-  void ssp(long[] s,int n,char d) { for(int i=0; i<n; ++i) System.out.print(s[i] + ""+ d); nl(); }
-  void ssp(long[] s,char d) { for(long e : s) System.out.print(e + ""+ d); nl(); }
-  void ssp(char[] s,int n,char d) { for(int i=0; i<n; ++i) System.out.print(s[i] + ""+ d); nl(); }
-  void ssp(char[] s,char d) { for(char e : s) System.out.print(e + ""+ d); nl(); }
-  void ssp(String[] s,int n,char d) { for(int i=0; i<n; ++i) System.out.print(s[i] + ""+ d); nl(); }
-  void ssp(String[] s,char d) { for(String e : s) System.out.print(e + ""+ d); nl(); }
-  void ssp(double[] s,int n,char d) { for(int i=0; i<n; ++i) System.out.print(s[i] + ""+ d); nl(); }
-  void ssp(double[] s,char d) { for(double e : s) System.out.print(e + ""+ d); nl(); }
-  int gcd(int a,int b) { return b!=0 ? gcd (b, a % b) : a; }
-  long gcd(long a,long b) { return b!=0 ? gcd (b, a % b) : a; }
-  int max(int a,int b) { return a > b ? a : b; }
-  long max(long a,long b) { return a > b ? a : b; }
-  int min(int a,int b) { return a < b ? a : b; }
-  long min(long a,long b) { return a < b ? a : b; }
-  void dedidec() {System.out.print
-  }
-}
+import java.util.*;
+
 class Main {
-  /*******************MAIN************************/
-  public static void main(String[] args) throws IOException {
-    Temp late = new Temp();
-    late.dedidec();
+
+  public static void main(String[] args) {
+    InputReader in = new InputReader(System.in);
+    PrintWriter w = new PrintWriter(System.out);
+    
+    w.close();
   }
-}
-/**********************FAST READER*********************/
-class FastReader {
-  BufferedReader br;
-  StringTokenizer st;
-  public FastReader() {
-    br = new BufferedReader(new InputStreamReader(System.in));
-  }
-  String next() {
-    while (st == null || !st.hasMoreElements()) {
-      try {
-        st = new StringTokenizer(br.readLine());
+  static class InputReader {
+
+    private final InputStream stream;
+    private final byte[] buf = new byte[8192];
+    private int curChar, snumChars;
+
+    public InputReader(InputStream st) {
+      this.stream = st;
+    }
+
+    public int read() {
+      if (snumChars == -1)
+        throw new InputMismatchException();
+      if (curChar >= snumChars) {
+        curChar = 0;
+        try {
+          snumChars = stream.read(buf);
+        } catch (IOException e) {
+          throw new InputMismatchException();
+        }
+        if (snumChars <= 0)
+          return -1;
       }
-      catch (IOException e) {
-        e.printStackTrace();
+      return buf[curChar++];
+    }
+
+    public int nextInt() {
+      int c = read();
+      while (isSpaceChar(c)) {
+        c = read();
       }
+      int sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = read();
+      }
+      int res = 0;
+      do {
+        res *= 10;
+        res += c - '0';
+        c = read();
+      } while (!isSpaceChar(c));
+      return res * sgn;
     }
-    return st.nextToken();
+
+    public long nextLong() {
+      int c = read();
+      while (isSpaceChar(c)) {
+        c = read();
+      }
+      int sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = read();
+      }
+      long res = 0;
+      do {
+        res *= 10;
+        res += c - '0';
+        c = read();
+      } while (!isSpaceChar(c));
+      return res * sgn;
+    }
+
+    public int[] nextIntArray(int n) {
+      int a[] = new int[n];
+      for (int i = 0; i < n; i++) {
+        a[i] = nextInt();
+      }
+      return a;
+    }
+
+    public String readString() {
+      int c = read();
+      while (isSpaceChar(c)) {
+        c = read();
+      }
+      StringBuilder res = new StringBuilder();
+      do {
+        res.appendCodePoint(c);
+        c = read();
+      } while (!isSpaceChar(c));
+      return res.toString();
+    }
+
+    public String nextLine() {
+      int c = read();
+      while (isSpaceChar(c))
+        c = read();
+      StringBuilder res = new StringBuilder();
+      do {
+        res.appendCodePoint(c);
+        c = read();
+      } while (!isEndOfLine(c));
+      return res.toString();
+    }
+
+    public boolean isSpaceChar(int c) {
+      return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    private boolean isEndOfLine(int c) {
+      return c == '\n' || c == '\r' || c == -1;
+    }
+
   }
-  int nextInt() { return Integer.parseInt(next()); }
-  long nextLong() { return Long.parseLong(next()); } 
-  double nextDouble() { return Double.parseDouble(next()); }
-  String nextLine() {
-    String str = "";
-    try {
-      str = br.readLine();
-    }
-    catch (IOException e) {
-      e.printStackTrace();
-    }
-    return str;
-  }
+
 }
