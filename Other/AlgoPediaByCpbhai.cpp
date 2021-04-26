1cae9a9667bb3179f484b7b28b769040a8fde195
Harshvardhan Singh <51012630+cpbhai@users.noreply.github.com>
2021-04-26 16:21:22 +0530

    Update AlgoPediaByCpbhai.cpp

--- Other/AlgoPediaByCpbhai.cpp
+++ Other/AlgoPediaByCpbhai.cpp
@@ -23,130 +23,265 @@ int main()
 }
 
 
-*************Graph Data Structure***************************
-class Vertex
-{
-	public:
-	int v, w;
-	Vertex(int vv,int ww) { v = vv; w = ww; }
+***********************Binary Indexed Tree Fendwick Tree**********
+#include<bits/stdc++.h>
+using namespace std;
+struct BIT{
+  static int N;
+  vector<int> arr;
+  vector<int> BT;
+  void init(int n){
+    N=n;
+    arr.resize(N);
+    BT.resize(N+1);
+  }
+  int sumOneTo(int R){//sums 1 to R
+    int res=0;
+    for(; R>0; R-=(R&(-R))) res+=BT[R];
+    return res;
+  }
+  int sumL2R(int L, int R){
+    return (sumOneTo(R) - sumOneTo(L-1));//hope func name and arg clears logic behind
+  }
+  void update(int i, int x){
+    arr[i-1]+=x;//updates in arr, below statement does the update in BT
+    for(; i<=N; i+=(i&(-i))) BT[i]+=x;//adds x, not replaces
+  }
+  void build(){
+    for(int i=0; i<N; ++i)
+      update(i+1, arr[i]);
+  }
 };
-map<int, vector<Vertex>> G;
-vector<bool> VIS(100);
-void add(int u, int v, int w = -1)
-{
-	assert(u >= 0 && v >= 0);//ensures +ve vertices
-	G[u].push_back(Vertex(v, w));
-	//G[v].push_back(Vertex(u, w));//comment if directed
+int BIT::N=0;//static initialization
+int main(){
+  int n;
+  cin>>n;
+  BIT bt;
+  bt.init(n);
+  for(int i=0; i<n; ++i) cin>>bt.arr[i];
+  bt.build();
+  while(true){
+    int q; cin>>q;
+    if(q==1){int R; cin>>R; cout<<bt.sumOneTo(R)<<"\n";}
+    else if(q==2){int L, R; cin>>L>>R; cout<<bt.sumL2R(L, R)<<"\n";}
+    else if(q==3){int onebasedindex, val; cin>>onebasedindex>>val; bt.update(onebasedindex, val);}
+    else break;
+  }
+  return 0;
 }
-void dfs(int v)
-{
-	if(VIS[v]) return;
-	cout << v << " ";
-	VIS[v] = 1;
-	for(Vertex e : G[v])
-	{
-		if(! VIS[e.v])
-		{
-			dfs(e.v);
-		}
-	}
+
+
+************************Segment Tree************************
+#include<bits/stdc++.h>
+using namespace std;
+struct SegTree{
+  static int N;
+  vector<int> arr;
+  vector<int> ST;
+  void init(int n){
+    N=n;
+    arr.resize(N); ST.resize(N*4);
+  }
+  void build(int ind, int low, int high){
+    if(low==high){
+      ST[ind]=arr[low];
+      return;
+    }
+    int mid=(low+high)/2;
+    build(2*ind+1, low, mid);
+    build(2*ind+2, mid+1, high);
+    ST[ind]=max(ST[2*ind+1], ST[2*ind+2]);
+  }
+  int query(int ind, int low, int high, int l, int r){
+    if(low>=l && high<=r) return ST[ind];//total overlap
+    if(r<low || high<l) return INT_MIN;//no overlap else go below for partial
+    int mid=(low+high)/2;
+    int left=query(2*ind+1, low, mid, l, r);
+    int right=query(2*ind+2, mid+1, high, l , r);
+    return max(left, right);
+  }
+};
+int SegTree::N=0;//static initialization
+int main(){;
+  SegTree sgt;
+  int n;
+  cin>>n;
+  sgt.init(n);
+  for(int i=0; i<n; ++i) cin>>sgt.arr[i];
+  sgt.build(0, 0, n-1);
+  cout<<sgt.query(0, 0, n-1, QueryFrom, QueryTo);
+  }
+  return 0;
 }
-void bfs(int v)
-{
-	queue<int> q;
-	q.push(v);
-	VIS[v] = 1;
-	while(! q.empty())
-	{
-		v = q.front();
-		q.pop();
-		cout << v << " ";
-		for(Vertex e : G[v])
-		{
- 			if(! VIS[e.v])
-			{
-				q.push(e.v);
-				VIS[e.v] = 1;
-			}
-		}
-	}
-}
-void resetVis(bool c)//arg is for newline
-{
-	if(c) cout << "\n";
-	for(int i = 0; i < 101; ++i) VIS[i] = false;
-}
-void cycDirUtil(int v, bool& found, bitset<100>& recStack)
-{
-	if(VIS[v])
-	{
-		if(recStack[v]) found = 1;
-		return;
-	}
-	VIS[v] = 1; recStack.set(v);
-	for(Vertex e : G[v])
-	{
-		if(e.v == v) { found = 1; continue; }//selfLoop
-		cycDirUtil(e.v, found, recStack);
-	}
-	recStack.reset(v);
-}
-bool cycleDirec()
-{
-	bool found = false;
-	bitset<100> recStack;
-	cycDirUtil(0, found, recStack);//starts from zero
-	return found;
-}
-void cycUndUtil(int v, int par, bool& found)
-{
-	if(VIS[v])
-	{
-		found = 1;
-		return;
-	}
-	VIS[v] = 1;
-	for(Vertex e : G[v])
-	{
-		if(e.v == v) { found = 1; continue; }//selfLoop
-		if(e.v != par)
-			cycUndUtil(e.v, v, found);
-	}
-}
-bool cycleUnd()
-{
-	bool found = false;
-	cycUndUtil(0, -1, found);//starts from zero
-	return found;
-}
-void utiltopSort(int v, stack<int>& st)
-{
-	if(VIS[v]) return;
-	VIS[v] = 1;
-	for(Vertex e : G[v])
-	{
-		utiltopSort(e.v, st);
-	}
-	st.push(v);
-}
-void topSort()
-{
-	stack<int> st;
-	for(int i = 0; i < 6; ++i)
-		if(! VIS[i])
-			utiltopSort(i, st);
-	while(! st.empty()) { cout << st.top() << " "; st.pop(); }
-	cout << "\n";
-}
-int main()
-{
-	for(int i = 0; i < 6; ++i)
-	{
-		int a, b;
-		cin >> a >> b;
-		add(a, b);
-	}
-	return 0;
+
+
+*************Graph Data Structure***************************
+#include<bits/stdc++.h>
+using namespace std;
+int INF=1e5+5;
+struct Graph{
+  int V;
+  vector<vector<pair<int, int>>> G;
+  vector<short> VIS;
+  void init(int v){
+    V=v;
+    G.resize(V);
+    VIS.resize(V);
+  }
+  void add(int u, int v, int w = -1){
+  	assert(u >= 0 && v >= 0);//ensures +ve vertices
+  	G[u].push_back({v,w});
+  	//G[v].push_back({u,w});//comment if directed
+  }
+  void dfs(int v){
+  	if(VIS[v]) return;
+  	cout << v << " ";
+  	VIS[v] = 1;
+  	for(auto e : G[v])
+  	{
+  		if(! VIS[e.first])
+  		{
+  			dfs(e.first);
+  		}
+  	}
+  }
+  void bfs(int v){
+  	queue<int> q;
+  	q.push(v);
+  	VIS[v] = 1;
+  	while(! q.empty())
+  	{
+  		v = q.front();
+  		q.pop();
+  		cout << v << " ";
+  		for(auto e : G[v])
+  		{
+   			if(! VIS[e.first])
+  			{
+  				q.push(e.first);
+  				VIS[e.first] = 1;
+  			}
+  		}
+  	}
+  }
+  void resetVis(bool c){//arg is for newline
+  	if(c) cout << "\n";
+  	fill(VIS.begin(), VIS.begin()+V, 0);
+  }
+  void utiltopSort(int v, stack<int>& st){
+  	if(VIS[v]) return;
+  	VIS[v] = 1;
+  	for(auto e : G[v])
+  	{
+  		utiltopSort(e.first, st);
+  	}
+  	st.push(v);
+  }
+  void topSort(){
+  	stack<int> st;
+  	for(int i = 0; i < V; ++i)
+  		if(! VIS[i])
+  			utiltopSort(i, st);
+  	while(! st.empty()) { cout << st.top() << " "; st.pop(); }
+  	cout << "\n";
+  }
+  bool utilcycDir(int v){
+  	if(VIS[v] == 1) return 1;
+  	if(VIS[v] == 2) return 0;
+  	VIS[v] = 1;
+  	for(auto e:G[v]){
+  		if(utilcycDir(e.first)) return 1;
+  	}
+  	VIS[v] = 2;
+  	return 0;
+  }
+  bool cycDir(){
+  	for(int i=0; i<V; ++i){
+  		if(! VIS[i] && utilcycDir(i))
+  			return 1;
+  	}
+  	return 0;
+  }
+  void priMst(){
+  	int src=0;
+  	vector<int> dist(V,INF);
+  	vector<int> par(V,-1);
+  	//VIS will be used to check whether a vertex is in MST.
+  	priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;//weight, vertex
+  	pq.push({0,src});
+  	VIS[src]=1;
+  	dist[src]=0;
+  	while(! pq.empty())
+  	{
+  		int u=pq.top().second;
+  		pq.pop();
+  		VIS[u]=1;
+  		for(auto e:G[u]){
+  			int v=e.first;
+  			int w=e.second;
+  			if(! VIS[v] && dist[v] > w){
+  				dist[v]=w;
+  				pq.push({dist[v],v});
+  				par[v]=u;
+  			}
+  		}
+  	}
+  	for(int i=0; i<V; ++i) cout<<par[i]<<"->"<<i<<"\n";
+  }
+  void dijkstras(int s){//arg is src
+  	vector<int> d (V, INF), p(V);
+  	d[s] = 0;
+  	for (int i=0; i<V; ++i) {
+  		int v = -1;
+  		for(int j=0; j<V; ++j)
+  			if (!VIS[j] && (v == -1 || d[j] < d[v]))
+  				v = j;
+  		if (d[v] == INF)
+  			break;
+  		VIS[v] = 1;
+
+  		for (size_t j=0; j<G[v].size(); ++j) {
+  			int to = G[v][j].first;
+  				int len = G[v][j].second;
+  			if (d[v] + len < d[to]) {
+  				d[to] = d[v] + len;
+  				p[to] = v;
+  			}
+  		}
+  	}
+  	for(int i=0; i<V; ++i) cout<<i<<"->"<<d[i]<<"\n";
+  }
+  void bellmanFord(int src){
+    vector<int> dist(V, INF);
+    dist[src]=0;
+    for(int u=0; u<V; ++u){
+      for(auto e:G[u]){
+        int v=e.first;
+        int w=e.second;
+        int tmpDist=dist[u]+w;
+        if(tmpDist < dist[v]){
+          dist[v]=tmpDist;
+        }
+      }
+    }
+    dist[src]=0;
+    for(int i=0; i<V; ++i) cout<<i<<"->"<<dist[i]<<"\n";
+  }
+};
+int main(){
+  Graph g;
+  int V;//size of Graph
+  cin >> V;
+  g.init(V);
+  int n;//no. of edges
+  cin >> n;
+  for(int i=0; i<n; ++i){
+    int u, v;
+    cin>>u>>v;
+    g.add(u, v);
+  }
+  cout << g.cycDir();
+  return 0;
 }
 
 
@@ -181,11 +316,11 @@ void buildSparseTable(vector<int>& arr, int& n)
         lookup[i][0] = arr[i];
     for (int j = 1; (1 << j) <= n; j++) {
         for (int i = 0; (i + (1 << j) - 1) < n; i++) {
-            if (lookup[i][j - 1] < 
+            if (lookup[i][j - 1] <
                         lookup[i + (1 << (j - 1))][j - 1])
                 lookup[i][j] = lookup[i][j - 1];
             else
-                lookup[i][j] = 
+                lookup[i][j] =
                          lookup[i + (1 << (j - 1))][j - 1];
         }
     }
@@ -253,11 +388,11 @@ int sol(vector<int> arr, int k) {
 
 
 ****************KMP Substring occurence find***************************
-vector<int> prefix_function(string &s) 
+vector<int> prefix_function(string &s)
 {
 	int n = s.size();
 	vector<int> pi(n);
-	for(int i = 1; i < n; i++) 
+	for(int i = 1; i < n; i++)
 	{
 		int j = pi[i - 1];
 		while(j > 0 && s[i] != s[j])
@@ -399,207 +534,4 @@ void solvethetestcase()
     delByVal(head, &head, 10);
     trvl2r(head);
 
-}
-*******************Template Done until 117 line no.*********************
-// --------------------------------------------------<TEMPLATE>-------------------------------------------------- 
-// --------------------<optimizations>-------------------- 
- 
-/* 
-#pragma GCC optimize("Ofast,unroll-loops") 
-#pragma GCC target("avx,avx2,fma") 
-*/  
- 
-// -------------------</optimizations>-------------------- 
-  
-// ---------------<Headers and namespaces>---------------- 
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-/* 
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds; 
-*/ 
-  
-// ---------------</Headers and namespaces>--------------- 
-  
-// -----------------<Defines and typedefs>---------------- 
-// typedef tree<int,null_type,less<int>,rb_tree_tag, \ 
-tree_order_statistics_node_update> indexed_set; // use less_equal for multiset 
-// order_of_key (val): returns the no. of values less than val 
-// find_by_order (k): returns the iterator to kth largest element.(0-based) 
-  
-typedef long double LD; 
-typedef long long ll; 
-#define int ll 
-#define pb push_back 
-#define mp make_pair 
-#define REP(i,n) for (int i = 0; i < n; i++) 
-#define FOR(i,a,b) for (int i = a; i < b; i++) 
-#define REPD(i,n) for (int i = n-1; i >= 0; i--) 
-#define FORD(i,a,b) for (int i = a; i >= b; i--) 
-#define remax(a,b) a = max(a,b) 
-#define remin(a,b) a = min(a,b) 
-#define all(v) v.begin(),v.end() 
-typedef map<int,int> mii; 
-typedef vector<int> vi; 
-typedef vector<vi> vvi; 
-typedef pair<int,int> pii; 
-typedef vector<pii> vpii; 
-#define F first 
-#define S second 
-#define PQ(type) priority_queue<type> 
-#define PQD(type) priority_queue<type,vector<type>,greater<type> > 
-#define WL(t) while(t --) 
-#define SZ(x) ((int)(x).size()) 
-#define runtime() ((double)clock() / CLOCKS_PER_SEC) 
-#define inrange(i,a,b) ((i>=min(a,b)) && (i<=max(a,b))) 
-  
-/* 
-// -----<SCANF>----- 
-#define sfi(x) scanf("%d",&x); 
-#define sfi2(x,y) scanf("%d%d",&x,&y); 
-#define sfi3(x,y,z) scanf("%d%d%d",&x,&y,&z); 
-  
-#define sfl(x) scanf("%lld",&x); 
-#define sfl2(x,y) scanf("%lld%lld",&x,&y); 
-#define sfl3(x,y,z) scanf("%lld%lld%lld",&x,&y,&z); 
-#define sfl4(x,y,z,x1) scanf("%lld%lld%lld%lld",&x,&y,&z,&x1); 
-#define sfl5(x,y,z,x1,y1) scanf("%lld%lld%lld%lld%lld",&x,&y,&z,&x1,&y1); 
-#define sfl6(x,y,z,x1,y1,z1) scanf("%lld%lld%lld%lld%lld%lld",&x,&y,&z,&x1,&y1,&z1); 
-  
-#define sfs(x) scanf("%s",x); 
-#define sfs2(x,y) scanf("%s%s",x,y); 
-#define sfs3(x,y,z) scanf("%s%s%s",x,y,z); 
-// ----</SCANF>----- 
-  
-// ----<PRINTF>----- 
-#define pfi(x) printf("%d\n",x); 
-#define pfi2(x,y) printf("%d %d\n",x,y); 
-#define pfi3(x,y,z) printf("%d %d %d\n",x,y,z); 
-  
-#define pfl(x) printf("%lld\n",x); 
-#define pfl2(x,y) printf("%lld %lld\n",x,y); 
-#define pfl3(x,y,z) printf("%lld %lld %lld\n",x,y,z); 
-  
-#define pfs(x) printf("%s\n",x); 
-#define pfs2(x,y) printf("%s %s\n",x,y); 
-#define pfs3(x,y,z) printf("%s %s %s\n",x,y,z); 
-  
-#define pwe(x) printf("%lld ",x); // print without end 
-// ----</PRINTF>---- 
-*/ 
- 
-#define FLSH fflush(stdout) 
-/*#define fileIO(name) \ 
-    freopen(name".in", "r", stdin); \ 
-    freopen(name".out", "w", stdout); */
-#define PRECISION(x) cout << fixed << setprecision(x);  
-#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
-  
-// ----------------</Defines and typedefs>---------------- 
-  
-// -------------------<Debugging stuff>------------------- 
-#define TRACE 
-  
-#ifdef TRACE 
-#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
-template <typename Arg1> 
-void __f(const char* name, Arg1&& arg1){ 
-    cout << name << " : " << arg1 << std::endl; 
-} 
-template <typename Arg1, typename... Args> 
-void __f(const char* names, Arg1&& arg1, Args&&... args){ 
-    const char* comma = strchr(names + 1, ',');cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...); 
-} 
-#else 
-#define trace(...) 
-#endif 
-  
-// ------------------</Debugging stuff>------------------- 
-  
-// ------------------------<Consts>----------------------- 
-const int MAXN = 1000005; 
-const int SQRTN = 1003; 
-const int LOGN = 22; 
-const double PI=acos(-1); 
-  
-#ifdef int 
-const int INF=1e16; 
-#else 
-const int INF=1e9; 
-#endif 
-  
-const int MOD = 1000000007; 
-const int FMOD = 998244353; 
-const double eps = 1e-9; 
- 
-// -----------------------</Consts>----------------------- 
- 
-/* 
- 
-// -------------------------<RNG>------------------------- 
-mt19937 RNG(chrono::steady_clock::now().time_since_epoch().count());  
-#define SHUF(v) shuffle(all(v), RNG); 
-// Use mt19937_64 for 64 bit random numbers. 
-  
-// ------------------------</RNG>------------------------- 
-  
-// ----------------------<MATH>--------------------------- 
- 
-template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);} 
-template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));} 
-int add(int a, int b, int c = MOD){int res=a+b;return(res>=c?res-c:res);} 
-int mod_neg(int a, int b, int c = MOD){int res;if(abs(a-b)<c)res=a-b;else res=(a-b)%c;return(res<0?res+c:res);} 
-int mul(int a, int b, int c = MOD){ll res=(ll)a*b;return(res>=c?res%c:res);} 
-int muln(int a, int b, int c = MOD){ll res=(ll)a*b;return ((res%c)+c)%c;} 
-ll mulmod(ll a,ll b, ll m = MOD){ll q = (ll)(((LD)a*(LD)b)/(LD)m);ll r=a*b-q*m;if(r>m)r%=m;if(r<0)r+=m;return r;} 
-template<typename T>T expo(T e, T n){T x=1,p=e;while(n){if(n&1)x=x*p;p=p*p;n>>=1;}return x;} 
-template<typename T>T power(T e, T n, T m = MOD){T x=1,p=e;while(n){if(n&1)x=mul(x,p,m);p=mul(p,p,m);n>>=1;}return x;} 
-template<typename T>T extended_euclid(T a, T b, T &x, T &y){T xx=0,yy=1;y=0;x=1;while(b){T q=a/b,t=b;b=a%b;a=t;\ 
-t=xx;xx=x-q*xx;x=t;t=yy;yy=y-q*yy;y=t;}return a;} 
-template<typename T>T mod_inverse(T a, T n = MOD){T x,y,z=0;T d=extended_euclid(a,n,x,y);return(d>1?-1:mod_neg(x,z,n));} 
-  
-const int FACSZ = 1; // Make sure to change this 
-  
-int fact[FACSZ],ifact[FACSZ]; 
-  
-void precom(int c = MOD){ 
-    fact[0] = 1; 
-    FOR(i,1,FACSZ) fact[i] = mul(fact[i-1],i,c); 
-    ifact[FACSZ-1] = mod_inverse(fact[FACSZ-1],c); 
-    REPD(i,FACSZ-1){ 
-        ifact[i] = mul(i+1,ifact[i+1],c); 
-    } 
-} 
-  
-int ncr(int n,int r,int c = MOD){ 
-    return mul(mul(ifact[r],ifact[n-r],c),fact[n],c); 
-}  
-*/ 
- 
-// ----------------------</MATH>-------------------------- 
-// --------------------------------------------------</TEMPLATE>-------------------------------------------------- 
-  
-void solvethetestcase(); 
-  
-signed main(){ 
-    // comment when using scanf,printf 
-    FAST_IO 
- 
-    int t = 1; 
-    // (UNCOMMENT FOR MULTIPLE TEST CASES) \ 
-    cin >> t; 
-    FOR(testcase,1,t+1){ 
-        // (UNCOMMENT FOR CODEJAM) \ 
-        printf("Case #%lld: ",testcase);  
-        solvethetestcase(); 
-    } 
-}    
-  
-int n; 
-  
-void solvethetestcase(){ 
-    cin >> n; 
- 
 }
